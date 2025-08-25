e74303ff261bb0987d8714bd0b9f034a66248aa0
youngrxstt <david.grishin.2000@bk.ru>
2025-08-26 00:17:12 +0600

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gg664/OneDrive/Рабочий стол/search_engine
-# Build directory: C:/Users/gg664/OneDrive/Рабочий стол/search_engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("lib")
-subdirs("tests")
--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gg664/OneDrive/Рабочий стол/search_engine/build/_deps/googletest-src
-# Build directory: C:/Users/gg664/OneDrive/Рабочий стол/search_engine/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
